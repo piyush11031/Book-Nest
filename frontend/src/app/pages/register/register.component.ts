9f1360bc94e564a45486a442fcf2ac4a696e12f0
piyush11031 <pareekpiyush747@gmail.com>
2024-08-16 17:37:30 +0530

    implemented activate account frontend

--- frontend/src/app/pages/register/register.component.ts
+++ frontend/src/app/pages/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {RegistrationsRequest} from "../../services/models/registrations-request";
+import {Router} from "@angular/router";
+import {AuthenticationService} from "../../services/services/authentication.service";
+import {TokenService} from "../../services/token/token.service";
 
 @Component({
   selector: 'app-register',
@@ -6,5 +10,33 @@ import { Component } from '@angular/core';
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  registerRequest: RegistrationsRequest = {email: '', firstName: '', lastName: '', password: ''};
+  errorMsg: Array<string> = [];
 
+  constructor(
+    private router: Router,
+    private authService: AuthenticationService
+  ){
+
+  }
+
+  login() {
+    this.router.navigate(['login']);
+  }
+
+  register() {
+    this.errorMsg = [];
+
+    this.authService.register({
+      body: this.registerRequest
+    }).subscribe({
+      next: () => {
+        this.router.navigate(['activate-account']);
+      },
+      error: err => {
+        this.errorMsg = err.error.validationErrors;
+      }
+    })
+
+  }
 }
